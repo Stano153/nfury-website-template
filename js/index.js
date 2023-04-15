0ad1ca9c3bb082456de1c56ae036283eb24deb13
Stanislav Ondrej <112212979+Stano153@users.noreply.github.com>
2023-04-15 23:08:50 +0200

    Adding back to top, light box for gallery images with jquery

--- js/index.js
+++ js/index.js
@@ -44,36 +44,81 @@ $(function headingAnimation()
     title2.slideUp(2000);
 });
 
+(function ($) {
 
-/*
+    let gallery = $('.gallery')
 
-$(function()
-{
-    let number = prompt("Enter number:");
-    let number2 = 3 * number;
-    console.log("Number is " + number2);
+    //opacity on hover
+    
+    gallery.find('img').css ({
+        opacity:0.8,
+    }).on('mouseenter mouseleave', function(event) {
+        
+
+        let opacity;
+        if ( event.type == 'mouseenter' ) opacity = 1;
+        else opacity = 0.8;
+
+        $(this).stop().fadeTo(200, opacity)
+    });
+
+    // --------------- OVERLAY
+
+    let overlay = $('<div/>', { id: 'overlay'});
+        overlay.appendTo('body').hide();
+
+    gallery.find('a').on('click', function(e) {
+        
+        let href = $(this).attr('href'),
+            image = $('<img>', { src: href, alt:'learn2code' });
+
+        overlay.html( image )
+                .show();
+
+        overlay.show();
+        e.preventDefault();
+    })
+
+    overlay.on('click', function() {
+        overlay.hide();
+    });
+
+    $(document).on('keyup', function(e) {
+        if (e.which === 27 ) overlay.hide();
+    });
+
+
+    // SMOOTH TABS SCROOL
+
+let tabs = $('.tabs'),
+tabLinks = tabs.find('a');
+
+tabLinks.on('click', function(e) {
+    $('html,body').animate({ scrollTop: $(this.hash).offset().top  }, 1000);
+    e.preventDefault();
 });
 
-*/
+    // SMOOTH BACK TO TOP SCROLL
 
-/*
+        let backToTop = $('<a>', {
+                href: '#home',
+                class: 'back-to-top',
+                html: '<i class="fa fa-caret-up fa-5x"></i>'
+        });
 
-//checking if sum of number1 and number2 is even or odd
+        backToTop
+            .hide()
+            .appendTo('body')
+            .on('click', function(e) {
+                e.preventDefault(); 
+                $('html,body').animate({ scrollTop: 0 }, 1000);
+        });
 
-$(function()
-{
-    let sign1 = +prompt("Enter number 1");
-    let sign2 = +prompt("Enter number 2");
-    let mainResult = sign1 + sign2;
-    
-    alert(`Result is ${mainResult}`);
-    if(mainResult % 2 == 0) {
-        alert("Number is even.");
-    }
-    
-    else {
-        alert("Number is odd.");
-    }
-   
-});
-*/
+        let win = $(window);
+            win.scroll(function () { 
+                if ( win.scrollTop() >= 500 ) backToTop.fadeIn();
+                else backToTop.hide();
+    });
+
+
+})(jQuery);
